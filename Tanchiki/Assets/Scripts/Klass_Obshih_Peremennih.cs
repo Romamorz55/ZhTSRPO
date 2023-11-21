e7166f5ebd57e6974b5fe71121f65c581f8b6d6d
Romamorz55 <romabg15@gmail.com>
2023-11-21 10:46:22 +0300

    null project add

---
+++ Tanchiki/Assets/Scripts/Klass_Obshih_Peremennih.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Klass_Obshih_Peremennih : MonoBehaviour
+{
+    public bool Povorot_Levo_Global = false;
+    public bool Povorot_Verh_Global = false;
+    public bool Povorot_Pravo_Global = false;
+    public bool Povorot_Niz_Global = false;
+    //-----------------------------------------
+
+    public float ugol_vert_Pushki;
+    public float ugol_horizont_Pushki;
+
+}
